3273239a535cf10621de4488a6a47b8e6901ff10
Shashwat Koliwad <koliwad96@gmail.com>
2019-03-13 10:56:07 +0530

    prepare gh-pages

---
+++ precache-manifest.f7ea6a03b84c55123ec028fa29e420f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a85afaab04600e454541",
+    "url": "/react-to-do-list/static/js/runtime~main.d9a6f0de.js"
+  },
+  {
+    "revision": "6d404b6e0f2f851126e0",
+    "url": "/react-to-do-list/static/js/main.dac17bc4.chunk.js"
+  },
+  {
+    "revision": "8fe7b98fa6630bcef4b6",
+    "url": "/react-to-do-list/static/js/2.14e89176.chunk.js"
+  },
+  {
+    "revision": "6d404b6e0f2f851126e0",
+    "url": "/react-to-do-list/static/css/main.04a44d76.chunk.css"
+  },
+  {
+    "revision": "72998716501f0dd6de3bc4a39daca687",
+    "url": "/react-to-do-list/index.html"
+  }
+];
